5b393ff519e90c0c72981a84eefb4996ad5cb330
Dan Taeyoung <provolot@gmail.com>
2021-03-17 14:46:05 -0400

    some transcript html for css edits

--- src/components/Transcript.vue
+++ src/components/Transcript.vue
@@ -5,7 +5,12 @@
     </div>
     <div class="content">
       <div class="header">Transcript</div>
-      <div class="text">Minho Kwon with Yoonji Kang, November</div>
+      <div class="transcript">
+        <div class="titlebar"><span class="title">Minho Kwon with Yoonji Kang</span>, <span class="date">November</span></div>
+        <p><span class="person">Minho:</span> Okay Yoonji, I’m glad to see you in person. I’m will ask you some question about our exhibition. The first question is, who and where are you?</p>
+        <p><span class="person">Yoonji:</span> First of all, nice to meet you as well. My name is Yoonji Kan from South Korea, and I am a graduate architecture student in RISD. I am taking classes fully online.</p>
+        <p><span class="person">Minho:</span> Oh, that’s really interesting because...</p>
+      </div>
     </div>
   </div>
 </template>
