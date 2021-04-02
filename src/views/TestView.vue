77bc29e6fc59587bba256f5acd6aa7391d97c92c
Dan Taeyoung <provolot@gmail.com>
2021-04-02 03:24:59 -0400

    vuex now cross-compares svg audiopaths with airtable data

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -13,27 +13,6 @@
 
 /*  eslint-disable */ 
 
-function loadAudiopathDataFromSvg(svgpath, callback) {
-  const parser = new DOMParser(); 
-  fetch(svgpath)
-    .then(function (response) {
-      return response.text();
-    })
-    .then(function(text) {
-      const svgDoc = parser.parseFromString(text, 'image/svg+xml');
-      let pathData = Array.from(svgDoc.querySelectorAll('path')).map(function(x) { return { rawid: x.getAttribute("id"), d: x.getAttribute("d") }  })
-
-      var audiopathData = pathData.filter(function(d) {
-          return d.rawid.includes( "PATH-");
-      });
-
-      audiopathData.forEach(function(d) {
-        d.id = d.rawid.replace("PATH-", "");
-      });
-
-      callback(audiopathData);
-    });
-}
 
 
 export default {
@@ -49,12 +28,6 @@ export default {
   },
   methods: {
     parse: function() {
-
-      loadAudiopathDataFromSvg(this.mapsvg, function(audiopathData) {
-
-        
-        console.log("got data", audiopathData);
-      });
 
     },
   },
